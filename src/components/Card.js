df06d2070bcb1a7f561610ef357f4fe700265fcf
TheArtemis <lordef01@gmail.com>
2024-08-03 02:36:04 +0200

    Fixed sizing issues on mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,27 @@
 import "../styles/card.css"
 
 function Card(props) {
-    
-    return (
+    if (props.thing === "The next thing will be revealed in: 00:00:00:00") {
+      return (
         <div className="card noselect">
             <div className="card-header">
                 <p className="number">#{props.number}</p>
             </div> 
-            <h1 className="thing">{props.thing}</h1>
+            <h1 className="thing reveal">Reveal</h1>
         </div>
-    );
+    );  
+    } else {
+    return (
+            <div className="card noselect">
+                <div className="card-header">
+                    <p className="number">#{props.number}</p>
+                </div> 
+                <h1 className="thing">{props.thing}</h1>
+            </div>
+        );
+    } 
+    
+    
 
 }
 
